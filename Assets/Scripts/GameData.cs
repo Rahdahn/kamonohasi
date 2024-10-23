5f44a11f2781ec452cb96cdbe9027b7ed1a263c7
Rahdahn <TTC2350SG0051@edu.tech.ac.jp>
2024-10-23 18:23:39 +0900

    ^^

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,11 +1,11 @@
 public static class GameData
 {
     // 動物の種類ごとの捕獲数
-    public static int D1Count { get; set; } = 0;
-    public static int D2Count { get; set; } = 0;
-    public static int D3Count { get; set; } = 0;
-    public static int D4Count { get; set; } = 0;
-    public static int D5Count { get; set; } = 0;
+    public static int D1Count { get; set; } = 2;
+    public static int D2Count { get; set; } = 2;
+    public static int D3Count { get; set; } = 2;
+    public static int D4Count { get; set; } = 2;
+    public static int D5Count { get; set; } = 2;
 
     // 捕獲数をリセットするメソッド
     public static void ResetCounts()
